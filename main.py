9a33dc54b277a7e31007a322320bb427fc99aa60
YuYuShenMing <117810457+YuYuShenMing@users.noreply.github.com>
2022-11-09 17:29:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,10 +35,11 @@ def get_birthday():
   return (next - today).days
 
 def get_words():
-  words = requests.get("https://www.mxnzp.com/api/daily_word/recommend?app_id=avmjhimmkqck7rpp&app_secret=SmFIemU1Ni9IR3R4NXFDbFg3NHB4UT09")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+  ##words = requests.get("https://api.shadiao.pro/chp")
+  url = "https://www.mxnzp.com/api/daily_word/recommend?app_id=avmjhimmkqck7rpp&app_secret=SmFIemU1Ni9IR3R4NXFDbFg3NHB4UT09"
+  ##if words.status_code != 200:
+    ## return get_words()
+  return words.json()['data']['content']
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
