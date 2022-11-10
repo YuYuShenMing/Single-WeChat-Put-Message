9a78004839b4fab802953d31845090a2d01c3f67
YuYuShenMing <117810457+YuYuShenMing@users.noreply.github.com>
2022-11-10 17:03:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature, low, high = get_weather()
-sure, hidden = getyq()
+sure, hidden = get_yq()
 data = {"weather":{"value":wea, "color":get_random_color()},"temperature":{"value":temperature, "color":get_random_color()},
         "low":{"value":low, "color":get_random_color()},"high":{"value":high, "color":get_random_color()},
         "love_days":{"value":get_count(), "color":get_random_color()},"birthday_left":{"value":get_birthday(), "color":get_random_color()},
