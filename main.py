a25927ecda48a10e1913d8356f917a06aab31397
YuYuShenMing <117810457+YuYuShenMing@users.noreply.github.com>
2022-11-11 14:56:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ birthday = os.environ['BIRTHDAY']
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
-user_id = os.environ["USER_ID"]["USER_IDTWO"]
-#user_id_2 = os.environ["USER_IDTWO"]
+user_id = os.environ["USER_ID"]
+user_id_2 = os.environ["USER_IDTWO"]
 template_id = os.environ["TEMPLATE_ID"]
 
 def get_city():
@@ -66,8 +66,8 @@ data = {"weather":{"value":wea, "color":get_random_color()},"temperature":{"valu
         "low":{"value":low, "color":get_random_color()},"high":{"value":high, "color":get_random_color()},
         "love_days":{"value":get_count(), "color":get_random_color()},"birthday_left":{"value":get_birthday(), "color":get_random_color()},
         "words":{"value":get_words(), "color":get_random_color()}, '''"sure":{"value":sure, "color":get_random_color()},"hidden":{"value":hidden, "color":get_random_color()},
-       "words2":{"value":get_words2(), "color":get_random_color()},''' "city":{"value":city, "color":get_random_color()}}
-res1 = wm.send_template(user_id, template_id, data)
-#res2 = wm.send_template(user_id_2, template_id, data)
-print(res1)
-#print(res2)
+       "words2":{"value":get_words2(), "color":get_random_color()},''' "city":{"value":city, "color":get_random_color()}, "date":{"value":today, "color":get_random_color()}}
+res = wm.send_template(user_id, template_id, data)
+res2 = wm.send_template(user_id_2, template_id, data)
+print(res)
+print(res2)
