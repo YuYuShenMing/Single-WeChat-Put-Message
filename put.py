17680d0485f5176a7a72aa04b09d981507861330
YuYuShenMing <117810457+YuYuShenMing@users.noreply.github.com>
2022-11-15 16:30:10 +0800

    Update put.py

--- put.py
+++ put.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime
+import datetime
 import math
 from wechatpy import WeChatClient
 from wechatpy.client.api import WeChatMessage, WeChatTemplate
@@ -7,18 +7,36 @@ import os
 import random
 import time
 
-
-today = datetime.now()
+today = datetime.datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
+
+jingqi = os.environ['JINGQI']
+jingqitoday = datetime.datetime.date(datetime.datetime.now())
+zhouqi = 26
+laterday = 2
+
+
+app_id = os.environ["APP_ID"]
+app_secret = os.environ["APP_SECRET"]
+
+user_id = os.environ["USER_ID"]
+template_id = os.environ["TEMPLATE_ID"]
+
+# app_id = 'wxb4afc17cd3aafdc6'
+# app_secret = 'f39f44b24136158129928cf7de792dc2'
+#
+# user_id = 'ohLxg5_WDNdN_zoLfiwNBez9awI0'
+# template_id = 'NUMnOmRtk_jiCfVX6RtOkD233aZCBS8YqfJKoD7aE64'
+
 
 # def get_city():
 #  return city
 
 def get_course():
     # 课程
-    weekindex = datetime.now().weekday()
+    weekindex = datetime.datetime.now().weekday()
     course = ''
     if weekindex == 1 or weekindex == 4 or weekindex == 6:
         course = "宝~今天要去练声喔~~"
@@ -28,11 +46,11 @@ def get_course():
 
 def get_date():
     # 当前日期
-    year = datetime.now().year
-    month = datetime.now().month
-    day = datetime.now().day
+    year = datetime.datetime.now().year
+    month = datetime.datetime.now().month
+    day = datetime.datetime.now().day
     weeklist = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
-    weekindex = datetime.now().weekday()
+    weekindex = datetime.datetime.now().weekday()
     week = weeklist[weekindex]
     time = str(year) + "年" + str(month) + "月" + str(day) + "日" + " " + str(week)
     return time
@@ -48,14 +66,14 @@ def get_weather():
 
 def get_count():
     # 时间统计
-    delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+    delta = today - datetime.datetime.strptime(start_date, "%Y-%m-%d")
     return delta.days
 
 
 def get_birthday():
     # 生日
-    next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-    if next < datetime.now():
+    next = datetime.datetime.strptime(str(datetime.date.today().year) + "-" + birthday, "%Y-%m-%d")
+    if next < datetime.datetime.now():
         next = next.replace(year=next.year + 1)
     return (next - today).days
 
@@ -67,18 +85,19 @@ def get_words():
         return get_words()
     return words.json()['data']['text']
 
-def get_jingqi(momday1, today, round, tempday):
+
+def get_jingqi(momday1, today, zhouqi, tempday):
     # 经期
     try:
         ym = 7
         mm_year = int(momday1.split("-")[0])
         mm_month = int(momday1.split("-")[1])
         mm_day = int(momday1.split("-")[2])
-        momday = date(mm_year, mm_month, mm_day)  # 当前日期
+        momday = datetime.date(mm_year, mm_month, mm_day)  # 当前日期
         sumdays = str(today.__sub__(momday)).split(" ")[0]  # 距上次日期
-        days = int(int(sumdays) / round)  # 周期
+        days = int(int(sumdays) / zhouqi)  # 周期
         TempDay = tempday * days  # 延迟日期
-        delta = datetime.timedelta(days=days * round + TempDay)  # 周期及延迟天数增量
+        delta = datetime.timedelta(days=days * zhouqi + TempDay)  # 周期及延迟天数增量
         startday = momday + delta  # 开始日期
         delta = datetime.timedelta(days=ym - 1)  # 最后一天增量
         lastday = startday + delta  # 最后一天
@@ -96,16 +115,16 @@ def get_jingqi(momday1, today, round, tempday):
             else:
                 dy = a - ym
             mytext = '预计下次姨妈还有' + str(dy) + '天~'
-    except:
-        mytext = ''
+    except Exception as e:
+        mytext = str(e)
     return mytext
 
+
 def get_random_color():
-    # 时间
+    # 颜色
     return "#%06x" % random.randint(0, 0xFFFFFF)
 
 # def get_yq():
-    # 疫情
 #  url = "https://covid.myquark.cn/quark/covid/data?city=" + city
 #  yq = requests.get(url).json()
 #  yq_data = yq['contryData']
@@ -115,3 +134,18 @@ def get_random_color():
 #  words2 = requests.get('http://open.iciba.com/dsapi/')
 #  return words2.json()['note']
 
+
+# client = WeChatClient(app_id, app_secret)
+#
+# wm = WeChatMessage(client)
+# wea, temperature, low, high = get_weather()
+# #sure, hidden = get_yq()
+# '''"sure":{"value":sure, "color":get_random_color()},"hidden":{"value":hidden, "color":get_random_color()},
+#        "words2":{"value":get_words2(), "color":get_random_color()},'''
+# data = {"weather":{"value":wea, "color":get_random_color()},"temperature":{"value":temperature, "color":get_random_color()},
+#         "low":{"value":low, "color":get_random_color()},"high":{"value":high, "color":get_random_color()},
+#         "love_days":{"value":get_count(), "color":get_random_color()},"birthday_left":{"value":get_birthday(), "color":get_random_color()},
+#         "words":{"value":get_words(), "color":get_random_color()}, "date":{"value":get_date(), "color":get_random_color()}, "city":{"value":city, "color":get_random_color()},
+#        "course":{"value":get_course(), "color":get_random_color()}}
+# res = wm.send_template(user_id, template_id, data)
+# print(res)
