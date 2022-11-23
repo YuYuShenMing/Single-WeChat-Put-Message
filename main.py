d8f015d94f335884f16b5b6e376a155310894856
YuYuShenMing <117810457+YuYuShenMing@users.noreply.github.com>
2022-11-23 16:38:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ data = {"weather": {"value": wea, "color": get_random_color()},
         "words": {"value": get_words(), "color": get_random_color()},
         "date": {"value": get_date(), "color": get_random_color()},
         "city": {"value": city, "color": get_random_color()},
-        "course": {"value": get_course(), "color": get_random_color()},
+        #"course": {"value": get_course(), "color": get_random_color()},
         "jingqi": {"value": jingqi_data, "color": get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
